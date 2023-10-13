17c05bcfcc5878caf6092e7d09330b6383dc245b
Kevin FEDYNA <fedyna.kevin@gmail.com>
2023-10-13 13:11:31 +0200

    simplest main file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
-import geometry.Vector2D;
 import graphics.Window;
 
 public class Main {
     public static void main(String[] args) {
-        Window test = new Window();
-        Vector2D position = new Vector2D();
+        new Window();
     }
 }
