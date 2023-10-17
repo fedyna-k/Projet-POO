19d6f6c13a2f8c03c3d15b0f8d952428d621a889
Kevin FEDYNA <fedyna.kevin@gmail.com>
2023-10-17 14:13:47 +0200

    Begin working on entity / player inheritance

--- src/main/java/character/Entity.java
+++ src/main/java/character/Entity.java
@@ -1,12 +1,18 @@
 package character;
 
 
+import java.awt.image.BufferedImage;
+
 import geometry.Vector2D;
 
 import graphics.Animation;
 
 
 public abstract class Entity {
+    public enum AnimationIndex {
+        STANDING, LEFTRUN, RIGHTRUN
+    };
+
     protected Vector2D coordinates;
     protected int height;
     protected int width;
@@ -17,20 +23,50 @@ public abstract class Entity {
     protected Animation leftRun;
     protected Animation rightRun;
 
+    /**
+     * Move Entity by a given vector
+     * @param dx x coordinate of vector
+     * @param dy y coordinate of vector
+     */
+    public void move(double dx, double dy) {
+        this.coordinates.x += dx;
+        this.coordinates.y += dy;
+    }
 
-    void setAnimations(String dir) {
+    /**
+     * Go through all basic animations and load them
+     * @param dir The folder contaning all frames
+     */
+    protected void setAnimations(String dir) {
         standing = Animation.load("standing", Animation.RESOURCES_FOLDER + dir, 10);
         leftRun = Animation.load("leftrun", Animation.RESOURCES_FOLDER + dir, 10);
         rightRun = Animation.load("rightrun", Animation.RESOURCES_FOLDER + dir, 10);
     }
 
     /**
-     * Move Entity by a given vector
-     * @param dx x coordinate of vector
-     * @param dy y coordinate of vector
+     * Get entity sprite to display
+     * @return Current sprite
      */
-    void move(double dx, double dy) {
-        this.coordinates.x += dx;
-        this.coordinates.y += dy;
+    public BufferedImage getSprite() {
+        return current.getCurrentFrame();
+    }
+
+    /**
+     * Change the animation to display
+     * If the animation is the same than before, no change is made
+     * @param animationIndex A constant index that describes the type of animation
+     */
+    public void swapAnimation(AnimationIndex animationIndex) {
+        this.current.stop();
+        
+        if (animationIndex == AnimationIndex.STANDING && this.current != this.standing) {
+            this.current = this.standing;
+        } else if (animationIndex == AnimationIndex.LEFTRUN && this.current != this.leftRun) {
+            this.current = this.leftRun;
+        } else if (animationIndex == AnimationIndex.RIGHTRUN && this.current != this.rightRun) {
+            this.current = this.rightRun;
+        }
+
+        this.current.play();
     }
 }
--- src/main/java/graphics/Canvas.java
+++ src/main/java/graphics/Canvas.java
@@ -1,32 +1,20 @@
 package graphics;
 
-import javax.swing.JPanel;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.HashSet;
-import java.util.Set;
 
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import geometry.Vector2D;
 
-
-public class Canvas extends JPanel implements KeyListener {
+public class Canvas extends JPanel {
     private boolean isFullscreen;
     private Timer timer;
 
-    private Animation current;
-    private Animation standing;
-    private Animation leftRun;
-    private Animation rightRun;
-    private Vector2D position;
-    private Vector2D movement;
-    private Set<Integer> allPressedKeys;
 
     public Canvas() {
         this(false);
@@ -35,63 +23,10 @@ public class Canvas extends JPanel implements KeyListener {
     public Canvas(boolean isFullscreen) {
         super(true);
         this.isFullscreen = isFullscreen;
-        this.allPressedKeys = new HashSet<Integer>();
-        this.position = new Vector2D(0, 100);
-        this.movement = new Vector2D(2, 1);
-
-        setFocusable(true);
-        addKeyListener(this);
-
-        (new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                System.out.println("Position : " + position.x + ", " + position.y);
-                System.out.println(allPressedKeys);
-            }
-        })).start();
-
-        standing = Animation.load("standing", Animation.RESOURCES_FOLDER + "player/", 10);
-        leftRun = Animation.load("leftrun", Animation.RESOURCES_FOLDER + "player/", 10);
-        rightRun = Animation.load("rightrun", Animation.RESOURCES_FOLDER + "player/", 10);
-        
-        current = standing;
-        current.play();
 
         timer = new Timer(0, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                if (position.x < 0 || position.x > 472) {movement.x *= -1;}
-                if (position.y < 0 || position.y > 472) {movement.y *= -1;}
-
-
-                if (allPressedKeys.contains(KeyEvent.VK_LEFT)) {
-                    movement.x -= 1;
-                }
-                if (allPressedKeys.contains(KeyEvent.VK_RIGHT)) {
-                    movement.x += 1;
-                }
-                if (allPressedKeys.contains(KeyEvent.VK_UP)) {
-                    movement.y -= 1;
-                }
-                if (allPressedKeys.contains(KeyEvent.VK_DOWN)) {
-                    movement.y += 1;
-                }
-                position = Vector2D.add(position, movement);
-
-                if ((movement.x > 0 || movement.x == 0 && movement.y != 0) && current != rightRun) {
-                    current.stop();
-                    current = rightRun;
-                    current.play();
-                } else if ((movement.x < 0) && current != leftRun) {
-                    current.stop();
-                    current = leftRun;
-                    current.play();
-                } else if (movement.x == 0 && movement.y == 0 && current != standing) {
-                    current.stop();
-                    current = standing;
-                    current.play();
-                }
-
                 repaint();
             }
         });
@@ -111,18 +46,7 @@ public class Canvas extends JPanel implements KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-        g.drawImage(current.getCurrentFrame(), (int)position.x, (int)position.y, 128, 128, this);
+        // To fix with character implementation
+        // g.drawImage(current.getCurrentFrame(), (int)position.x, (int)position.y, 128, 128, this);
     }
-    
-    public void keyPressed(KeyEvent e) {
-        System.out.println("Pressed : " + e.getKeyCode());
-        allPressedKeys.add(e.getKeyCode());
-    }
-
-    public void keyReleased(KeyEvent e) {
-        System.out.println("Released : " + e.getKeyCode());
-        allPressedKeys.remove(e.getKeyCode());
-    }
-
-    public void keyTyped(KeyEvent e) {}
 }
