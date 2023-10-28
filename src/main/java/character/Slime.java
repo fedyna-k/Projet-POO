36bd417361c05206710ac75b884c40f1305c4541
ARAB-Ryan <ryan.arab@etu.univ-amu.fr>
2023-10-29 00:47:34 +0200

    Ajout de propriete et de methode a la classe Slime

--- src/main/java/character/Slime.java
+++ src/main/java/character/Slime.java
@@ -1,4 +1,46 @@
 package character;
 
-public class Slime {
+import geometry.Vector2D;
+
+// Classe représentant un petit monstre Slime dans le jeu
+public class Slime extends Entity{
+    // Nom du Slime
+    private String nom;
+
+    // Points de vie actuels du Slime
+    private int pointDeVie;
+
+    // Force d'attaque du Slime
+    private int force;
+
+    // Constructeur pour initialiser un Slime avec un nom, des points de vie et une force donnés
+    public Slime(String nom, int pointDeVie, int force) {
+        this.nom = nom;
+        this.pointDeVie = pointDeVie;
+        this.force = force;
+    }
+
+    // Méthodes d'accès pour obtenir et définir le nom du Slime
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    // Méthodes d'accès pour obtenir et définir les points de vie du Slime
+    public int getPointDeVie() {
+        return pointDeVie;
+    }
+
+    public void setPointDeVie(int pointDeVie) {
+        this.pointDeVie = pointDeVie;
+    }
+
+    // Méthode abstraite à implémenter pour obtenir le décalage du Slime
+    @Override
+    public Vector2D getOffset() {
+        return null;
+    }
 }
