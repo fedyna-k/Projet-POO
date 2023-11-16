84f803a2a5bd6214ba18031eb00a4662440c46d9
ARAB-Ryan <ryan.arab@etu.univ-amu.fr>
2023-11-16 14:44:03 +0100

    update Mage.java

--- src/main/java/character/Mage.java
+++ src/main/java/character/Mage.java
@@ -25,33 +25,6 @@ public class Mage extends Entity {
         this.isFacingLeft = false;
     }
 
-
-
-    /*
-        // Constructor to initialize a mage with a given name, hit points, and force
-        public mage(String name, int lifePoints, int force) {
-            this.name = name;
-            setLifePoints(lifePoints); // Using the method from the Entity class to initialize hit points
-            setForce(force); // Using the method from the Entity class to initialize force
-        }
-
-        // Method to simulate the mage's attack
-        public void attack() {
-            System.out.println("The mage attacks with a force of " + getForce());
-        }
-
-        // Method to simulate the damage received by the mage and manage its state accordingly
-        public void receiveDamage(int damage) {
-            int remainingLifePoints = getLifePoints() - damage;
-            if (remainingLifePoints <= 0) {
-                setLifePoints(0); // If the damage exceeds the hit points, the hit points are set to 0
-                System.out.println("The mage is defeated!");
-            } else {
-                setLifePoints(remainingLifePoints); // Reduce the hit points based on the received damage
-                System.out.println("The mage now has " + getLifePoints() + " hit points");
-            }
-        }
-    */
     /**
      * Abstract method to implement to get the offset of the mage.
      * Adjusts the offset based on whether the mage is attacking and facing left.
