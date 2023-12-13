90f86012e29f80eefa0b396b6346c2d12cd65005
fedyna-k <fedyna.kevin@gmail.com>
2023-12-13 11:01:52 +0100

    Add better informations on debug and correct shell file

--- comprun.sh
+++ comprun.sh
@@ -1,21 +1,21 @@
 # Delete all previously compiled files
 if [[ "$@" =~ --del-all ]]; then
     echo -e '\033[0;31mSuppressions des fichiers class...\033[0m'
-    rm -rf bin/
+    sudo rm -rf bin/
 fi
 
 
 # Check if test or main project is going to be compiled
 if [[ "$@" =~ --test ]]; then
     echo -e '\033[0;33mCompilation projet test...\033[0m'
-    cd src/test/java
+    cd src/test/java/
 else
     echo -e '\033[0;33mCompilation projet principal...\033[0m'
-    cd src/main/java
+    cd src/main/java/
 fi
 
 # Proceed to compile and launch main
 javac -d ../../../bin *.java
-cd ../../../bin
+cd ../../../bin/
 java Main
 cd ..
